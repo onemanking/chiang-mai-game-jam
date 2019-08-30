4254328a0c534c5224bd15577a34653514226e9b
Rachen Tanimkarn <onemanking@hotmail.com>
2019-08-30 23:49:58 +0700

    [Changed] CharacterBase, OfficerBase and PrisonerBase class and adjust sampleScene

---
+++ GameJam/Assets/Scripts/OfficerBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficerBase : CharacterBase
+{
+	[SerializeField] protected float attackRange;
+
+	protected override bool IsCanAttack(string _tag, out CharacterBase _target)
+	{
+		Debug.DrawLine(transform.position, transform.TileDown());
+		var hits = Physics2D.LinecastAll(transform.position, transform.TileDown());
+		foreach (var hit in hits)
+		{
+			if (hit.collider)
+			{
+				if (hit.collider.gameObject != gameObject)
+				{
+					_target = hit.collider.CompareTag(_tag) ? hit.collider.GetComponent<CharacterBase>() : null;
+					return _target != null;
+				}
+				else
+				{
+					continue;
+				}
+			}
+			else
+			{
+				continue;
+			}
+		}
+
+		_target = null;
+		return false;
+	}
+}
