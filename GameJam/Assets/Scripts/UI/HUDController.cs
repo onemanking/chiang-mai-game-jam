d99710e7d5989908d79ffed43fc872e9ea81c1dc
SupawichDechpuwarat <de.supawich_st@tni.ac.th>
2019-09-01 16:25:06 +0700

    Build

--- GameJam/Assets/Scripts/UI/HUDController.cs
+++ GameJam/Assets/Scripts/UI/HUDController.cs
@@ -7,11 +7,17 @@ public class HUDController : MonoBehaviour
     public GameObject PuaseScreen;
     public void puase(){
         PuaseScreen.SetActive(true);
+
+        Time.timeScale = 0;
     }
     public void resume(){
         PuaseScreen.SetActive(false);
+
+        Time.timeScale = 1;
     }
     public void MainMenu(){
         SceneManager.LoadScene("mainMenu");
+
+        Time.timeScale = 1;
     }
 }
