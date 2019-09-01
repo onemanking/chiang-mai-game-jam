e2a8fc7fbdfc706a8e905514af20f8e9426e3aeb
jay <cchayanin@hotmail.com>
2019-09-01 15:28:02 +0700

    resume pause

--- GameJam/Assets/Scripts/UI/HUDController.cs
+++ GameJam/Assets/Scripts/UI/HUDController.cs
@@ -4,30 +4,11 @@ using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour
 {
-    [SerializeField] private Image[] Background;
-
-    [SerializeField] private Image[] skillImage;
-
-    private float[] cooldown = {0f,0f,0f,0f};
-    private float[] cooldownRate = {0.75f,0.5f,0.42f,0.35f};
-    void Awake()
-    {
-        //CGlobal_SkillManager.RegisterButtonSkill(this);
+    public GameObject PuaseScreen;
+    public void puase(){
+        PuaseScreen.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        for (int i = 0; i < cooldown.Length; i++)
-        {
-           if(cooldown[i] > 1){
-               skillImage[i].color = new Color32(255,255,255,255);
-           }
-           cooldown[i] += cooldownRate[i]*Time.deltaTime/2;
-        }
-        for (int i = 0; i < cooldown.Length; i++)
-        {
-            Background[i].fillAmount = cooldown[i];
-        }
+    public void resume(){
+        PuaseScreen.SetActive(false);
     }
 }
